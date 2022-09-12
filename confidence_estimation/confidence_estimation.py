6744b933068b0e187c67524e425bfc6b1329e0cc
Tomer Ronen <tomer.ronen34@gmail.com>
2022-09-12 20:06:55 +0300

    saving metrics

--- confidence_estimation/confidence_estimation.py
+++ confidence_estimation/confidence_estimation.py
@@ -1,17 +1,59 @@
-from openai.openai_object import OpenAIObject
+import json
+from pathlib import Path
+
 import numpy as np
-from confidence_estimation.confidence_estimation import extract_quoted_tokens
-from utils import sacrebleu
 import openai
+from fire import Fire
+from openai.openai_object import OpenAIObject
+from tqdm import tqdm
+from time import sleep
+
+from utils import sacrebleu
 
 DEUTCH_WRITE_A_SENTENCE = "Schreibe einen Satz: "
 
 
-def main(model_name: str = "text-babbage-001", num_examples: int = 10):
+def main(model_name: str = "text-curie-001", num_examples: int = 200):
+    data_dir = Path(
+        "/home/olab/tomerronen1/data/fairseq-mcrerank/dr_nmt_paper/parallel/text_data/clean_detok")
+    dump_dir = Path(__file__).parent / "metrics"
+    dump_dir.mkdir(exist_ok=True, parents=True)
+    dump_path = dump_dir / \
+        f"AAA_wmt19_de_to_en__{model_name}__{num_examples}_examples__metrics.json"
+
+    english_sentences = _read_lines(data_dir / "valid.en")
+    deutch_sentences = _read_lines(data_dir / "valid.de")
+
     openai_caller = OpenAICaller(model_name=model_name)
 
-    bleu_score = sacrebleu(pred=predicted_text, label=label)
-    return avg_input_logprob, avg_precited_logprob, bleu_score
+    metrics = {
+        "i_example": [],
+        "pred_perplexity": [],
+        "echo_perplexity": [],
+        "bleu_score": [],
+    }
+    example_indices = np.random.RandomState(seed=1337).permutation(
+        len(english_sentences))[:num_examples]
+    for i_example in tqdm(example_indices):
+        pred_text, pred_perplexity, echo_perplexity = openai_caller.predict(
+            deutch_sentences[i_example])
+        sleep(5)
+        bleu_score = sacrebleu(
+            pred=pred_text, label=english_sentences[i_example])
+
+        metrics["i_example"].append(int(i_example))
+        metrics["pred_perplexity"].append(pred_perplexity)
+        metrics["echo_perplexity"].append(echo_perplexity)
+        metrics["bleu_score"].append(bleu_score)
+
+    with open(dump_path, 'w') as f:
+        json.dump(metrics, f, indent=2)
+
+
+def _read_lines(path: Path) -> list[str]:
+    text = path.read_text()
+    lines = text.strip().split('\n')
+    return lines
 
 
 class OpenAICaller:
@@ -24,14 +66,11 @@ class OpenAICaller:
         self.task_prompt = task_prompt
         self.echo_prompt = echo_prompt
 
-# input_text = "schauspieler orlando bloom und model miranda kerr wollen künftig getrennte wege gehen."
-# label = "actors orlando bloom and model miranda kerr want to go their separate ways."
     def predict(self, input_text: str) -> tuple[str, float, float]:
         inference_response = self._make_inference_call(input_text)
         echo_response = self._make_echo_call(input_text)
         pred_text, pred_perplexity = _process_response(inference_response)
         echoed_input_text, echo_perplexity = _process_response(echo_response)
-        assert echoed_input_text == input_text
         return pred_text, pred_perplexity, echo_perplexity
 
     def _make_inference_call(self, input_text: str) -> OpenAIObject:
@@ -53,7 +92,7 @@ class OpenAICaller:
 
 
 def _process_response(response: OpenAIObject) -> tuple[str, float]:
-    tokens, logprobs = extract_quoted_tokens(response)
+    tokens, logprobs = _extract_quoted_tokens(response)
     text = ''.join(tokens)
     avg_logprob = np.mean(logprobs)
     return text, avg_logprob
@@ -74,3 +113,7 @@ def _extract_quoted_tokens(response: OpenAIObject, drop_tokens_with_null_logprob
         logprobs = logprobs[1:]
 
     return tokens, logprobs
+
+
+if __name__ == "__main__":
+    Fire(main)
