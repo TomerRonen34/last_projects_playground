8b17691d6ff6e90c787ff8bc5d4648a36794f3f5
Tomer Ronen <tomer.ronen34@gmail.com>
2022-12-08 02:59:52 +0200

    metrics calculation in mbart50_translate.py

--- confidence_estimation/mbart50_translate.py
+++ confidence_estimation/mbart50_translate.py
@@ -1,15 +1,21 @@
-import math
-from typing import Union
+from functools import partial
 from pathlib import Path
-from transformers.models.mbart50.tokenization_mbart50_fast import FAIRSEQ_LANGUAGE_CODES
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, DataCollatorForSeq2Seq
-from datasets import Dataset
-from typing import Optional
-from tqdm import tqdm
-import numpy as np
-from torch import Tensor
+from typing import Optional, Union
+
+import bert_score
 import jsonlines
+import numpy as np
+from bert_score import BERTScorer
+from datasets import Dataset
 from fire import Fire
+from torch import Tensor
+from tqdm import tqdm
+from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer,
+                          DataCollatorForSeq2Seq)
+from transformers.models.mbart50.tokenization_mbart50_fast import \
+    FAIRSEQ_LANGUAGE_CODES
+from utils import (batch_indices, dict_of_lists_to_list_of_dicts, read_lines,
+                   rouge, sacrebleu)
 
 LANG_CODE_TO_FAIRSEQ_FORMAT = {
     long_language_code[:2]: long_language_code for long_language_code in FAIRSEQ_LANGUAGE_CODES}
@@ -18,16 +24,18 @@ LANG_CODE_TO_FAIRSEQ_FORMAT = {
 class MBart50Translator:
     """
     python -m mbart50_translate --device="cpu" --num_examples=10 --batch_size=2 --data_dir="data/wmt20" --dump_dir="mbart50_dumps" --src_lang="en" --tgt_lang="de"
-    GPUS=1 run_with_slurm gen $(which run_python_script) -m mbart50_translate --data_dir="data/wmt20" --dump_dir="mbart50_dumps" --src_lang="en" --tgt_lang="ru"
+    GPUS=1 run_with_slurm gen $(which run_python_script) -m mbart50_translate  --num_examples=10 --data_dir="data/wmt20" --dump_dir="mbart50_dumps" --src_lang="en" --tgt_lang="ru"
+    GPUS=1 run_with_slurm gen $(which run_python_script) -m mbart50_translate  --num_examples=10 --data_dir="data/wmt20" --dump_dir="mbart50_dumps" --src_lang="ru" --tgt_lang="en"
     """
 
     def __init__(self,
                  data_dir: str,
-                 dump_dir: str,
                  src_lang: str,
                  tgt_lang: str,
+                 dump_dir: str = "mbart50_dumps",
                  num_examples: Optional[int] = 200,
                  batch_size: int = 4,
+                 bertscore_batch_size: Optional[int] = None,
                  device: str = "cuda:0",
                  dataset_name: str = "wmt20",
                  num_beams: int = 5,
@@ -43,13 +51,15 @@ class MBart50Translator:
         self.tgt_lang = tgt_lang
         self.num_examples = num_examples
         self.batch_size = batch_size
+        self.bertscore_batch_size = bertscore_batch_size if bertscore_batch_size is not None else batch_size
         self.device = device
         self.dataset_name = dataset_name
         self.max_output_to_input_ratio = max_output_to_input_ratio
 
-        self.dump_path = Path(
+        self.generations_dump_path = Path(
             dump_dir) / f"{dataset_name}_{src_lang}-{tgt_lang}_{num_examples}examples.jsonl"
-        assert not self.dump_path.exists()
+        self.metrics_dump_path = self.generations_dump_path.with_suffix(
+            ".with_metrics.jsonl")
 
         if generate_multiple_options == "auto":
             generate_multiple_options = (self.src_lang == "en")
@@ -69,15 +79,20 @@ class MBart50Translator:
         self.data_collator = DataCollatorForSeq2Seq(self.tokenizer)
 
     def __call__(self) -> None:
+        self._run_generation()
+        self._run_metrics_calculation()
+
+    def _run_generation(self) -> None:
+        assert not self.generations_dump_path.exists()
         dataset = self._prepare_dataset()
-        num_batches = math.ceil(len(dataset) / self.batch_size)
-        batches = np.array_split(np.arange(len(dataset)), num_batches)
-        for batch_indices in tqdm(batches, desc="generating"):
-            batch = dataset[batch_indices]
+        batches = batch_indices(len(dataset), self.batch_size)
+        for indices in tqdm(batches, desc="generating"):
+            batch = dataset[indices]
             gen_results = self._generate(batch)
-            self._dump_batch(gen_results)
-            # metrics = self._calculate_metrics(
-            #     gen_results["gen_texts"], batch["tgt_sequence"])
+            inflated_batch = {k: np.repeat(
+                v, self.num_return_sequences).tolist() for k, v in batch.items()}
+            to_dump = {**inflated_batch, **gen_results}
+            self._append_to_jsonlines_file(to_dump, self.generations_dump_path)
 
     def _generate(self, batch: dict) -> dict[list]:
         input_ids, attention_mask = self._prepare_batch_for_generation(batch)
@@ -96,9 +111,8 @@ class MBart50Translator:
         scores = gen_output.sequences_scores.tolist()
         texts = self.tokenizer.batch_decode(
             gen_output.sequences, skip_special_tokens=True)
-        src_ids = np.repeat(batch["id"], self.num_return_sequences).tolist()
         gen_results = {"gen_sequence": sequences,
-                       "gen_score": scores, "gen_text": texts, "src_id": src_ids}
+                       "gen_score": scores, "gen_text": texts}
         return gen_results
 
     def _prepare_batch_for_generation(self, batch: dict) -> tuple[Tensor, Tensor]:
@@ -109,10 +123,10 @@ class MBart50Translator:
         input_ids, attention_mask = model_inputs["input_ids"], model_inputs["attention_mask"]
         return input_ids, attention_mask
 
-    def _dump_batch(self, to_dump: dict[list]) -> None:
-        self.dump_path.parent.mkdir(exist_ok=True, parents=True)
+    def _append_to_jsonlines_file(self, to_dump: dict[list], path: Path) -> None:
+        path.parent.mkdir(exist_ok=True, parents=True)
         to_dump = dict_of_lists_to_list_of_dicts(to_dump)
-        with jsonlines.open(self.dump_path, 'a') as jsonl_writer:
+        with jsonlines.open(path, 'a') as jsonl_writer:
             jsonl_writer.write_all(to_dump)
 
     def _prepare_dataset(self, seed: int = 10) -> Dataset:
@@ -134,21 +148,42 @@ class MBart50Translator:
         tgt_sentences = read_lines(str(base_path) + ".ref")
         return src_sentences, tgt_sentences
 
+    def _run_metrics_calculation(self) -> None:
+        assert not self.metrics_dump_path.exists()
+        if hasattr(self, "model"):
+            del self.model
+        if not hasattr(self, "bert_scorer"):
+            self.bert_scorer = self._load_bert_scorer()
 
-def read_lines(path: Union[Path, str]) -> list[str]:
-    text = Path(path).read_text()
-    lines = text.split('\n')
-    while lines[-1] == '':
-        lines = lines[:-1]
-    return lines
+        gen_dataset = Dataset.from_json(str(self.generations_dump_path))
 
+        batches = batch_indices(len(gen_dataset), self.bertscore_batch_size)
+        for indices in tqdm(batches, desc="calculating metrics"):
+            batch = gen_dataset[indices]
+            gen_texts, tgt_sentences = batch["gen_text"], batch["tgt_sentence"]
+            precisions, recalls, f1s = self.bert_scorer.score(
+                cands=gen_texts, refs=tgt_sentences)
+            metrics_results = {"bertscore_f1": f1s.tolist(),
+                               "bertscore_precision": precisions.tolist(),
+                               "bertscore_recall": recalls.tolist()}
+            metrics_results["bleu_score"] = [
+                sacrebleu(pred=gen, label=tgt) for gen, tgt in zip(gen_texts, tgt_sentences)]
+            for rouge_key in ("rougeL", "rouge2", "rouge1"):
+                rouge_scores = [rouge(pred=gen, label=tgt, rouge_key=rouge_key)
+                                for gen, tgt in zip(gen_texts, tgt_sentences)]
+                metrics_results[f"{rouge_key}_score"] = rouge_scores
+            to_dump = {**batch, **metrics_results}
+            self._append_to_jsonlines_file(to_dump, self.metrics_dump_path)
 
-def dict_of_lists_to_list_of_dicts(d: dict[list]) -> list[dict]:
-    return [dict(zip(d.keys(), vals)) for vals in zip(*d.values())]
-
-
-def flatten(nested_list: list[list]) -> list:
-    return [item for sublist in nested_list for item in sublist]
+    def _load_bert_scorer(self) -> BERTScorer:
+        bertscore_baseline_languages = [path.name for path in (
+            Path(bert_score.__file__).parent / "rescale_baseline").iterdir()]
+        rescale_with_baseline = (self.tgt_lang in bertscore_baseline_languages)
+        model_type = "microsoft/deberta-xlarge-mnli" if self.tgt_lang == "en" else None
+        bert_scorer = BERTScorer(model_type=model_type, lang=self.tgt_lang,
+                                 rescale_with_baseline=rescale_with_baseline, device=self.device)
+        bert_scorer.score(['a', ], ['a'])  # make sure no exception is thrown
+        return bert_scorer
 
 
 if __name__ == "__main__":
--- confidence_estimation/utils.py
+++ confidence_estimation/utils.py
@@ -1,10 +1,43 @@
-from torchmetrics.functional import sacre_bleu_score, rouge_score
+import math
+import re
+from pathlib import Path
+from typing import Sequence, Union
+
+import numpy as np
+from torchmetrics.functional import rouge_score, sacre_bleu_score
 
 
-def sacrebleu(pred: str, label: str) -> float:
+def sacrebleu(*, pred: str, label: str) -> float:
     return sacre_bleu_score(preds=[pred], target=[[label]], lowercase=True).item()
 
 
-def rouge(pred: str, label: str, rouge_key: str = "rougeL") -> float:
-    return rouge_score(preds=pred, target=label, rouge_keys=rouge_key)[f"{rouge_key}_fmeasure"].item()
+def rouge(*, pred: str, label: str, rouge_key: str = "rougeL") -> float:
+    return rouge_score(preds=pred, target=label, rouge_keys=rouge_key, normalizer=_text_normalizer
+                       )[f"{rouge_key}_fmeasure"].item()
 
+
+def _text_normalizer(text: str) -> str:
+    """ the default behavior in rouge_score keeps only [a-z0-9] characters """
+    return re.sub(r'\W', ' ', text.lower())
+
+
+def read_lines(path: Union[Path, str]) -> list[str]:
+    text = Path(path).read_text()
+    lines = text.split('\n')
+    while lines[-1] == '':
+        lines = lines[:-1]
+    return lines
+
+
+def dict_of_lists_to_list_of_dicts(d: dict[list]) -> list[dict]:
+    return [dict(zip(d.keys(), vals)) for vals in zip(*d.values())]
+
+
+def flatten(nested_list: list[list]) -> list:
+    return [item for sublist in nested_list for item in sublist]
+
+
+def batch_indices(n: int, batch_size: int) -> Sequence[Sequence[int]]:
+    num_batches = math.ceil(n / batch_size)
+    batches = np.array_split(np.arange(n), num_batches)
+    return batches
